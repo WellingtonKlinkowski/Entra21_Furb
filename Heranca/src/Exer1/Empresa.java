644dc106312041000b4aa269f7d2bd52ccb81444
Wellington Klinkowski <wellimgtomklim@gmail.com>
2022-06-03 14:37:44 -0300

    Adicionando arquivos Diagrama de Classes e Exercícios do mesmo

--- Heranca/src/Exer1/Empresa.java
+++ Heranca/src/Exer1/Empresa.java
@@ -17,23 +17,49 @@ public class Empresa {
 				System.out.println("Quando entrou na empresa?");
 				colab.setDataIngresso(input.nextInt());
 				
+				System.out.println("Você é 1) Funcionário ou 2) Chefe?");
+				int cargo = input.nextInt();
+				
+				
+				Funcionario colab1 = new Funcionario(cargo, cargo);
+				
+				if(cargo == 1) {
+					System.out.println("Qual sua carga horária?");
+					colab1.setCargaHoraria(input.nextDouble());
+					System.out.println("Qual seu ID do sindicato?");
+					colab1.setIdSindicato(input.nextInt());
+					
+				}
+				
+				System.out.println("Desejar sair do sindicato? 1-Sim | 2-Não");
+				int escInd = input.nextInt();
+				if(escInd == 1) {
+					colab1.sairSindicato();
+					System.out.println("Vc saiu do sindicato e seu ID é " + colab1.getIdSindicato());
+				}
+
+				Chefe colab2 = new Chefe(cargo);
+				if(cargo == 2) {
+					System.out.println("Quantos subordinados vc tem?");
+					colab2.setQtdSubordinado(input.nextInt());	
+				}
+				
 				System.out.println("Deseja se demitir? 1-Sim | 2-Não");
 				int escolha = input.nextInt();
 				if(escolha == 1) {
 				colab.demitir();
 				}
 			
-				Funcionario colab1 = new Funcionario(escolha, escolha);
-				System.out.println("Qual sua carga horária?");
-				colab1.setCargaHoraria(input.nextDouble());
-				
-				System.out.println("Desejar sair do sindicato? 1-Sim | 2-Não");
-				colab1.setIdSindicato(input.nextInt());
-				if(colab1.getIdSindicato() == 1) {
-					colab1.sairSindicato();
-					System.out.println("Vc saiur do sindicato e seu ID é " + colab1.getIdSindicato());
-				}
-				
+			System.out.println("Seu nome: " + colab.getNome());
+			System.out.println("Seu salário: " + colab.getSalario());
+			System.out.println("Ingressou em: " + colab.getDataIngresso());
+			if(cargo == 1) {
+				System.out.println("Sua carga horária é: " + colab1.getCargaHoraria());
+				System.out.println("Seu ID do sindicato é: " + colab1.getIdSindicato());
+			}else {
+				System.out.println("O número de subordinados é: " + colab2.getQtdSubordinado());
+				System.out.println("Seu salário passa a ser: " + colab2.getSalario() * 2);
+			}
 
 				
 				
