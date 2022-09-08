77a554158675dd4ff1fb2e05dc7717bb07b8f46b
Wellington Klinkowski <wellimgtomklim@gmail.com>
2022-09-08 16:12:49 -0300

    Adicionando arquivos Angular

--- HTML/meuAppAngular/src/app/app.module.ts
+++ HTML/meuAppAngular/src/app/app.module.ts
@@ -8,14 +8,22 @@ import { HeaderComponent } from './components/templates/header/header.component'
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/templates/footer/footer.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { NavComponent } from './components/templates/nav/nav.component';
+import { HomeComponent } from './components/views/home/home.component';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
+    MatIconModule,
+    MatSidenavModule,
     MatToolbarModule,
     BrowserModule,
     AppRoutingModule,
